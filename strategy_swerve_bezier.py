b55e7906c62aacbc2bc10571172b47c3b78b5e5d
Andy Tomaszewski <zueski@gmail.com>
2024-11-07 00:20:40 -0500

    working pretty well

--- strategy_swerve_bezier.py
+++ strategy_swerve_bezier.py
@@ -23,27 +23,26 @@ def calc_bezier(t, p1, p2, p3, p4):
 		+ 3.0 * (t * t) * (1.0 - t) * p3
 		+ (t ** 3) * p4)
 
+def home_point(target, curr, max_speed):
+	target = target.asCart()
+	curr = curr.asCart()
+	x = target.x - curr.x
+	x = min(max(x, -max_speed), max_speed)
+	y = min(max(target.y - curr.y, -max_speed), max_speed)
+	return CartVertex2D(x, y)
+
 
 def strategy_simple(state):
-	dist = state["bot_loc"].get_distance(state["tag_loc"])
-	speed = dist / 10 +0.01
-	t = min(max(0.0, float(state["frame"]) / 375.0), 1.0)
-	#print("t:  ", t)
-	#~ print("b1: ",  ((1.0-t)**2.0)*p1.x)
-	#~ print("b2: ",  (2.0*t*(1.0-t)**2.0)*p2.x)
-	#~ print("b3: ",  3.0*(t**2.0)*p3.x + t**3)
-	#~ print("b4: ",  t**3 * p4.x)
-	
+	t = min(max(0.0, float(state["frame"]) / 75.0), 1.0)
 	x = calc_bezier(t, p1.x, p2.x, p3.x, p4.x)
 	y = calc_bezier(t, p1.y, p2.y, p3.y, p4.y)
 	target = CartVertex2D(x, y)
-	print(f"t: {t}   {target}")
 	angle = state["tag_loc"].asPolar().addVector(CartVertex2D(-state["bot_loc"].x, -state["bot_loc"].y)).asPolar().a
-	rotation = ( state["tag_pose"].asPolar().a - state["bot_pose"].asPolar().a) * 0.10
-	print(f" from {state['bot_loc'].asPolar()} to {state['tag_loc'].asPolar()} got angle {angle:.3f} rotation {rotation}")
-	print( target.asPolar().diffVector(state["bot_loc"]).asCart())
-	return target.asPolar().diffVector(state["bot_loc"]), rotation
+	rotation = ( state["tag_pose"].asPolar().a - state["bot_pose"].asPolar().a) * t
+	print(f" from {state['bot_loc'].asPolar()} to {state['tag_loc'].asPolar()} got angle {angle:.3f} rotation {rotation:.3f}")
+	print(f"t: {t:.3f}   {target} - {state['bot_loc']} => {target.asPolar().diffVector(state['bot_loc']).asCart()}")
+	return home_point(target, state["bot_loc"], 1.0), rotation
 
-ani = FuncAnimation(fig, partial(animate_holonomic, state=curr_state, strategy=strategy_simple), interval=300)
+ani = FuncAnimation(fig, partial(animate_holonomic, state=curr_state, strategy=strategy_simple), interval=30)
 
 run()
